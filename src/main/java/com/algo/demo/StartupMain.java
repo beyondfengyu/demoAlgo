0f701779e549b9675e22aad8c72336638e73aea3
laocy <594981878@qq.com>
2019-07-03 20:47:35 +0800

    增加排序算法的测试代码；

--- src/main/java/com/algo/demo/StartupMain.java
+++ src/main/java/com/algo/demo/StartupMain.java
@@ -12,32 +12,36 @@ public class StartupMain {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
 
+            int[] socArr = NumberUtil.genIntegerArr(10000, 1, 3000);
 
-            int[] socArr = NumberUtil.genIntegerArr(100000000, 1, 3000);
+            // 冒泡排序，注意数组长度不要太大，否则耗时很长
+            int[] bubbleArr = Arrays.copyOf(socArr, socArr.length);
+            BubbleSort bubbleSort = new BubbleSort();
+            bubbleSort.swapPrint(bubbleArr, true);
 
-//        int[] bubbleArr = Arrays.copyOf(socArr, socArr.length);
-//        BubbleSort bubbleSort = new BubbleSort();
-//        bubbleSort.swapPrint(bubbleArr, true);
-//
-//        int[] selectArr = Arrays.copyOf(socArr, socArr.length);
-//        SelectionSort selectionSort = new SelectionSort();
-//        selectionSort.swapPrint(selectArr, false);
-//
-//        int[] insertArr = Arrays.copyOf(socArr, socArr.length);
-//        InsertSort insertSort = new InsertSort();
-//        insertSort.swapPrint(insertArr, false);
+            // 选择排序，注意数组长度不要太大，否则耗时很长
+            int[] selectArr = Arrays.copyOf(socArr, socArr.length);
+            SelectionSort selectionSort = new SelectionSort();
+            selectionSort.swapPrint(selectArr, false);
 
+            // 插入排序，注意数组长度不要太大，否则耗时很长
+            int[] insertArr = Arrays.copyOf(socArr, socArr.length);
+            InsertSort insertSort = new InsertSort();
+            insertSort.swapPrint(insertArr, false);
+
+            // 归并排序
             int[] mergeArr = Arrays.copyOf(socArr, socArr.length);
             MergeSort mergeSort = new MergeSort();
             mergeSort.swapPrint(mergeArr, false);
 
+            // 快速排序，建议数组元素范围放大点，比如：[1-1000000]
             int[] quickArr = Arrays.copyOf(socArr, socArr.length);
             QuickSort quickSort = new QuickSort();
             quickSort.swapPrint(quickArr, false);
         }
 
 
-        System.out.println("===========================");
+        System.out.println("============================");
     }
 
 }
