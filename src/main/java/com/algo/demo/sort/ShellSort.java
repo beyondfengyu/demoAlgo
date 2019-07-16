6e366cc4da50a221fbf83b6d2693791bc72f9ecd
laocy <594981878@qq.com>
2019-07-16 17:35:51 +0800

    增加希尔排序算法；

--- src/main/java/com/algo/demo/sort/ShellSort.java
+++ src/main/java/com/algo/demo/sort/ShellSort.java
@@ -4,6 +4,7 @@ package com.algo.demo.sort;
 /**
  * 希尔排序
  * 时间复杂度：
+ * 稳定性：不稳定
  */
 public class ShellSort extends BaseSort {
 
@@ -15,10 +16,10 @@ public class ShellSort extends BaseSort {
 
         // 增量由len/2开始，每次减半，逐步缩小增量，
         // 循环对每一种增量的分组进行插入排序
-        for(int i = len/2; i > 0; i /= 2) {
+        for(int grap = len/2; grap > 0; grap /= 2) {
             // 此处变量 i 的值为增量
-            for (int j = i; j < len; j++) {
-                insertI(socArr, j, i);
+            for (int j = grap; j < len; j++) {
+                insertSort(socArr, j, grap);
             }
 
         }
@@ -29,17 +30,19 @@ public class ShellSort extends BaseSort {
      *
      * @param socArr
      * @param start
-     * @param incr    增量
+     * @param grap    增量
      */
-    public void insertI(int[] socArr, int start, int incr) {
+    private void insertSort(int[] socArr, int start, int grap) {
         int tmp = socArr[start];
         int i;
         // 循环查找该增量数组中比原值大的值
         // 即进行普通插入排序
-        for (i = start - incr; i >= 0 && socArr[i] > tmp; i -= incr) {
-            socArr[i + incr] = socArr[i];
+        for (i = start - grap; i >= 0 && socArr[i] > tmp; i -= grap) {
+            socArr[i + grap] = socArr[i];
         }
 
-        socArr[i + incr] = tmp;
+        socArr[i + grap] = tmp;
     }
+
+
 }
